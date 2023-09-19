72019fbf9e48c53cc371ddf06a9ffd45cf16dc9c
Khizar Baig Mohammed <59732957+Khizar-Baig@users.noreply.github.com>
2023-09-19 00:29:23 -0500

    Add files via upload

---
+++ CASIA1 VGG19.py
@@ -0,0 +1,306 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+import numpy as np
+import os
+import itertools
+import matplotlib.pyplot as plt
+import json
+import seaborn as sns
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, classification_report
+from keras.models import Sequential, load_model
+from keras.layers import Dense, Flatten, Conv2D, MaxPool2D, Dropout, Activation, GlobalAveragePooling2D
+from keras.optimizers import Adam
+from keras.callbacks import EarlyStopping
+from PIL import Image, ImageChops, ImageEnhance
+from tqdm.notebook import tqdm
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications import VGG19
+from tensorflow.keras.metrics import Precision, Recall
+from tensorflow.keras.callbacks import ReduceLROnPlateau
+
+
+# In[2]:
+
+
+#converts input image to ela applied 
+def convert_to_ela_image(path,quality):
+
+    original_image = Image.open(path).convert('RGB')
+
+    #resaving input image at the desired quality
+    resaved_file_name = 'resaved_image.jpg'     #predefined filename for resaved image
+    original_image.save(resaved_file_name,'JPEG',quality=quality)
+    resaved_image = Image.open(resaved_file_name)
+
+    #pixel difference between original and resaved image
+    ela_image = ImageChops.difference(original_image,resaved_image)
+    
+    #scaling factors are calculated from pixel extremas
+    extrema = ela_image.getextrema()
+    max_difference = max([pix[1] for pix in extrema])
+    if max_difference ==0:
+        max_difference = 1
+    scale = 350.0 / max_difference
+    
+    #enhancing elaimage to brighten the pixels
+    ela_image = ImageEnhance.Brightness(ela_image).enhance(scale)
+
+    ela_image.save("ela_image.png")
+    return ela_image
+
+
+# In[3]:
+
+
+def prepare_image(image_path):
+    image_size = (128, 128)
+    return np.array(convert_to_ela_image(image_path, 90).resize(image_size)).flatten() / 255.0         
+#normalizing the array values o
+
+
+# In[4]:
+
+
+X = [] # ELA converted images
+Y = [] # 0 for fake, 1 for real
+
+
+# In[5]:
+
+
+path = r'C:\Users\Admin\Downloads\archive (3)\CASIA1\Au'       #folder path of the authentic images in the dataset
+for filename in tqdm(os.listdir(path),desc="Processing Images : "):
+    if filename.endswith('jpg') or filename.endswith('png'):
+        full_path = os.path.join(path, filename)
+        X.append(prepare_image(full_path))        
+        Y.append(1)     # label for authentic images 
+        
+print(f'Total images: {len(X)}\nTotal labels: {len(Y)}')
+
+
+# In[6]:
+
+
+#adding forged images
+
+path = r'C:\Users\Admin\Downloads\archive (3)\CASIA1\Sp'       #folder path of the forged images in the dataset
+for filename in tqdm(os.listdir(path),desc="Processing Images : "):
+    if filename.endswith('jpg') or filename.endswith('png'):
+        full_path = os.path.join(path, filename)
+        X.append(prepare_image(full_path))        
+        Y.append(0)     # label for forged images 
+        
+print(f'Total images: {len(X)}\nTotal labels: {len(Y)}')
+
+
+# In[7]:
+
+
+X = np.array(X)
+Y = np.array(Y)
+X = X.reshape(-1, 128, 128, 3)
+
+
+# In[8]:
+
+
+# Training : Validation : Testing = 76 : 19 : 5
+X_temp, X_test, Y_temp, Y_test = train_test_split(X, Y, test_size = 0.05, random_state=5)
+X_train, X_val, Y_train, Y_val = train_test_split(X_temp, Y_temp, test_size = 0.2, random_state=5)
+X = X.reshape(-1,1,1,1)
+
+print(f'Training images: {len(X_train)} , Training labels: {len(Y_train)}')
+print(f'Validation images: {len(X_val)} , Validation labels: {len(Y_val)}')
+print(f'Test images: {len(X_test)} , Test labels: {len(Y_test)}')
+
+
+# In[11]:
+
+
+def build_model():
+    base_model = VGG19(weights='imagenet', include_top=False, input_shape=(128, 128, 3))
+    
+    model = Sequential()
+    model.add(base_model)
+    model.add(GlobalAveragePooling2D())
+    model.add(Dense(1, activation='sigmoid'))
+    
+    return model
+
+
+# In[12]:
+
+
+model = build_model()
+model.summary()
+
+
+# In[13]:
+
+
+epochs = 15
+batch_size = 32
+
+
+# In[14]:
+
+
+#Optimizer
+init_lr = 1e-4   #learning rate for the optimizer
+optimizer = Adam(lr = init_lr, decay = init_lr/epochs) 
+model.compile(optimizer = optimizer, loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+
+# In[15]:
+
+
+#Early Stopping
+early_stopping = EarlyStopping(monitor = 'val_accuracy',
+                               min_delta = 0,
+                               patience = 10,
+                               verbose = 0,
+                               mode = 'auto')
+
+
+# In[16]:
+
+
+hist = model.fit(X_train,
+                 Y_train,
+                 batch_size = batch_size,
+                 epochs = epochs,
+                 validation_data = (X_val, Y_val),
+                 callbacks = [early_stopping])
+
+
+# In[17]:
+
+
+#save the model as a h5 file
+model.save('.h7') 
+
+# get the dictionary containing each metric and the loss for each epoch
+history_dict = hist.history
+
+# save it as a json file
+json.dump(history_dict, open('model_history_7', 'w'))
+
+
+# In[18]:
+
+
+fig, ax = plt.subplots(1,2,figsize=(15,5))
+
+#Figure 1
+ax[0].plot(history_dict['loss'], color='b', label = "Training loss")
+ax[0].plot(history_dict['val_loss'], color='r', label = "Validation loss",axes =ax[0])
+ax[0].set_xlabel('Epochs',fontsize=16)
+ax[0].set_ylabel('Loss',fontsize=16)
+legend = ax[0].legend(loc='best', shadow=True)
+
+#Figure 2
+ax[1].plot(history_dict['accuracy'], color='b', label = "Training accuracy")
+ax[1].plot(history_dict['val_accuracy'], color='r',label = "Validation accuracy")
+ax[1].set_xlabel('Epochs',fontsize=16)
+ax[1].set_ylabel('Accuracy',fontsize=16)
+legend = ax[1].legend(loc='best', shadow=True)
+
+fig.suptitle('Metrics',fontsize=20);
+
+
+# In[19]:
+
+
+def plot_confusion_matrix(cf_matrix):
+  
+    group_counts = ["{0:0.0f}".format(value) for value in cf_matrix.flatten()] #number of images in each classification block
+    group_percentages = ["{0:.2%}".format(value) for value in cf_matrix.flatten()/np.sum(cf_matrix)] #percentage value of images in each block w.r.t total images
+
+    axes_labels=['Forged', 'Authentic']
+    labels = [f"{v1}\n{v2}" for v1, v2 in zip(group_counts,group_percentages)]
+    labels = np.asarray(labels).reshape(2,2)
+    sns.heatmap(cf_matrix, annot=labels, fmt='',cmap="flare" , xticklabels=axes_labels, yticklabels=axes_labels)
+
+    plot_xlabel = plt.xlabel('Predicted labels', fontsize = 13)
+    plot_ylabel = plt.ylabel('True labels', fontsize = 13)
+    plot_title = plt.title('Confusion Matrix', fontsize= 10,fontweight='bold')
+
+
+# In[20]:
+
+
+Y_pred = model.predict(X_val)               # Predict the values from the validation dataset 
+Y_pred_classes = np.round(Y_pred)           # roundoff the sigmoid value
+Y_true = Y_val                             
+
+confusion_mtx = confusion_matrix(Y_true, Y_pred_classes)     # compute the confusion matrix
+plot_confusion_matrix(confusion_mtx)                         # plot the confusion matrix
+
+
+# In[21]:
+
+
+print(classification_report(Y_true, Y_pred_classes))
+
+
+# In[22]:
+
+
+class_names = ['Forged', 'Authentic']
+
+
+# In[23]:
+
+
+# Testing accuracy
+correct_test = 0 #correctly predicted test images
+total_test = 0   #total test images
+
+for index,image in enumerate(tqdm(X_test,desc="Processing Images : ")):
+    image = image.reshape(-1, 128, 128, 3)
+    y_pred = model.predict(image)
+    y_pred_class = np.round(y_pred)
+    total_test += 1
+    if y_pred_class == Y_test[index]: #if prediction is correct
+        correct_test += 1
+    
+print(f'Total test images: {total_test}\nCorrectly predicted images: {correct_test}\nAccuracy: {correct_test / total_test * 100.0} %')
+
+
+# In[24]:
+
+
+test_image_path = r'C:\Users\Admin\Downloads\image_3.jpg'    # test image path
+test_image = prepare_image(test_image_path)
+test_image = test_image.reshape(-1, 128, 128, 3)
+
+y_pred = model.predict(test_image)
+y_pred_class = round(y_pred[0][0])
+
+fig, ax = plt.subplots(1,2,figsize=(15,5)) 
+
+#display original image
+original_image = plt.imread(test_image_path) 
+ax[0].axis('off')
+ax[0].imshow(original_image)
+ax[0].set_title('Original Image')
+
+#display ELA applied image
+ax[1].axis('off')
+ax[1].imshow(convert_to_ela_image(test_image_path,90)) 
+ax[1].set_title('ELA Image')
+
+print(f'Prediction: {class_names[y_pred_class]}')
+if y_pred<=0.5:
+    print(f'Confidence:  {(1-(y_pred[0][0])) * 100:0.2f}%')
+else:
+    print(f'Confidence: {(y_pred[0][0]) * 100:0.2f}%')
+print('--------------------------------------------------------------------------------------------------------------')
+
